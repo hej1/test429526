df94b070e2efe7a23c00de624becbd45e1b52483
unknown <hj429526@163.com>
2020-04-22 13:38:13 +0800

    第二次提交

---
+++ Demo/src/immoc/com/Shijian.java
@@ -0,0 +1,13 @@
+package immoc.com;
+import java.util.Date;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+public class Shijian {
+
+	public static void main(String[] args) {
+    	 Date d = new Date();
+    	 System.out.println(d);
+	}
+
+}
