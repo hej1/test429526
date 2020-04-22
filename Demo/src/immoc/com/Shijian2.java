6eb0937c950d0c0c5f0981ef9e10f6d1f487575e
unknown <hj429526@163.com>
2020-04-22 22:17:23 +0800

    Shijian2

---
+++ Demo/src/immoc/com/Shijian2.java
@@ -0,0 +1,13 @@
+package immoc.com;
+import java.util.Date;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+public class Shijian2 {
+
+	public static void main(String[] args) {
+    	 Date d = new Date();
+    	 System.out.println(d);
+	}
+
+}
